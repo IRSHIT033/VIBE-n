1ad87f30d2b71bbe1c473a555cf47beae9c3cb13
IRSHIT033 <mukherjeeirshit50@gmail.com>
2024-03-15 18:41:16 +0530

    fix: font error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,20 +4,10 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ChatProvider from './Context/ChatProvider';
 
-const theme = extendTheme({
-  styles: {
-    global: () => ({
-      body: {
-        fontFamily: 'cursive',
-      },
-    }),
-  },
-});
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ChatProvider>
-      <ChakraProvider theme={theme}>
+      <ChakraProvider>
         <App />
       </ChakraProvider>
     </ChatProvider>
