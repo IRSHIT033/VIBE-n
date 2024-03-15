8284d5c36ef4fc20bcde2fd1f09e00bd8d56634a
IRSHIT033 <mukherjeeirshit50@gmail.com>
2024-03-15 21:58:33 +0530

    fix : websocket only support in socket io

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,3 +1,5 @@
 import { io } from 'socket.io-client';
 
-export const socket = io(import.meta.env.VITE_SERVER_URL);
+export const socket = io(import.meta.env.VITE_SERVER_URL, {
+  transports: ['websocket'],
+});
