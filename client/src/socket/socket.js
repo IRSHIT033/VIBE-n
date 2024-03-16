6548d8cd6b5acb064388dd225d614b4ac12c7899
IRSHIT033 <mukherjeeirshit50@gmail.com>
2024-03-16 21:11:20 +0530

    fixed websockets

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,5 +1,3 @@
 import { io } from 'socket.io-client';
 
-export const socket = io(import.meta.env.VITE_SERVER_URL, {
-  transports: ['websocket'],
-});
+export const socket = io(import.meta.env.VITE_SERVER_URL);
